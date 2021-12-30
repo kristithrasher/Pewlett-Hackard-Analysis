3a2748ea4b68571dbd32564efe07d2dc3a6ed2e9
kristi thrasher <kristithrasher@yahoo.com>
2021-12-30 11:34:36 -0500

    pushing all new files to repository

--- schema.sql
+++ schema.sql
@@ -1,10 +1,6 @@
 -- Drop Tables containing FOREIGN KEYS using DROP CASCADE
--- DROP TABLE departments CASCADE;
--- DROP TABLE employees CASCADE;
-DROP TABLE dept_emp CASCADE;
-DROP TABLE dept_manager CASCADE;
-DROP TABLE salaries CASCADE;
-DROP TABLE titles CASCADE;
+--DROP TABLE tablename;
+
 
 -- Creating tables for PH-EmployeeDB
 -- departments table
@@ -69,3 +65,34 @@ CREATE TABLE titles (
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 	PRIMARY KEY (emp_no, title, from_date)
 );
+SELECT e.emp_no, 
+    e.first_name, 
+    e.last_name,
+    t.title,
+    t.from_date,
+    t.to_date
+INTO retirement_titles 
+FROM employees as e
+INNER JOIN titles as t
+ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY e.emp_no;  
+    
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (emp_no) emp_no,
+first_name,
+last_name,
+title
+INTO unique_titles
+FROM ;
+ORDER BY emp_no ASC, to_date DESC; 
+
+--SELECT * FROM unique_titles;
+--RETRIEVE number of employees by their most recent job tilte who are about to retire
+SELECT COUNT (title),title
+INTO retiring_titles
+FROM unique_titles
+GROUP BY title
+ORDER BY COUNT(title)DESC;
+
+SELECT * FROM retiring_titles;
